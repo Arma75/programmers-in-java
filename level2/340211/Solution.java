5cd44762678b2626289fcd1080cc170bdf9f7584
head <head@headui-MacBookAir.local>
2025-07-01 19:56:49 +0900

    solve

--- level2/340211/Solution.java
+++ level2/340211/Solution.java
@@ -1,110 +1,70 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/388353
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.LinkedList;
-
-class Point {
-    public int x;
-    public int y;
-    
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+// https://school.programmers.co.kr/learn/courses/30/lessons/340211
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
-    public void bfs(String[][] warehouse, String target) {
-        int warehouseHeight = warehouse.length;
-        int warehouseWidth = warehouse[0].length;
+    public List<int[]> getRobotPositionList(int[][] points, int[][] routes, int robotNum) {
+        List<int[]> positionList = new ArrayList<>();
+        int index = routes[robotNum][0] - 1;
+        int y = points[index][0];
+        int x = points[index][1];
         
-        boolean[][] outsides = new boolean[warehouseHeight][warehouseWidth];
-        boolean[][] visits = new boolean[warehouseHeight][warehouseWidth];
-        int[] dx = new int[]{1, -1, 0, 0};
-        int[] dy = new int[]{0, 0, 1, -1};
+        positionList.add(new int[]{y, x});
         
-        Queue<Point> deleteList = new LinkedList<>();
-
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0));
-
-        while( !queue.isEmpty() ) {
-            Point p = queue.poll();
-
-            if( visits[p.y][p.x] ) {
-                continue;
-            }
-
-            outsides[p.y][p.x] = true;
-            visits[p.y][p.x] = true;
-
-            for( int i = 0; i < 4; i++ ) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-
-                if( nx < 0 || nx >= outsides[0].length || ny < 0 || ny >= outsides.length ) {
-                    continue;
-                }
+        for( int i = 1; i < routes[robotNum].length; i++ ) {
+            int nextIndex = routes[robotNum][i] - 1;
+            int nextY = points[nextIndex][0];
+            int nextX = points[nextIndex][1];
             
-                if( target.equals(warehouse[ny][nx]) ) {
-                    deleteList.offer(new Point(nx, ny));
-                }
-                
-                if( visits[ny][nx] ) {
-                    continue;
-                } else if( !".".equals(warehouse[ny][nx]) ) {
-                    continue;
-                }
-
-                queue.offer(new Point(nx, ny));
+            while( y != nextY ) {
+                y += nextY - y > 0? 1 : -1;
+                positionList.add(new int[]{y, x});
+            }
+            
+            while( x != nextX ) {
+                x += nextX - x > 0? 1 : -1;
+                positionList.add(new int[]{y, x});
             }
         }
         
-        while( !deleteList.isEmpty() ) {
-            Point p = deleteList.poll();
-            warehouse[p.y][p.x] = ".";
-        }
+        return positionList;
     }
-
-    public int solution(String[] storage, String[] requests) {
+    
+    public int solution(int[][] points, int[][] routes) {
         int answer = 0;
         
-        int warehouseHeight = storage.length + 2;
-        int warehouseWidth = storage[0].length() + 2;
-        String[][] warehouse = new String[warehouseHeight][warehouseWidth];
-        
-        Arrays.fill(warehouse[0], ".");
-        Arrays.fill(warehouse[warehouseHeight - 1], ".");
-        for( int i = 0; i < storage.length; i++ ) {
-            String[] containers = ("." + storage[i] + ".").split("");
-            System.arraycopy(containers, 0, warehouse[i + 1], 0, warehouseWidth);
+        List<int[]>[] robotsPositionList = new ArrayList[routes.length];
+        for( int i = 0; i < routes.length; i++ ) {
+            robotsPositionList[i] = getRobotPositionList(points, routes, i);
         }
         
-        
-        for( String request : requests ) {
-            if( request.length() == 1 ) {
-                this.bfs(warehouse, request);
-            } else {
-                String target = request.charAt(0) + "";
-                for( int i = 1; i < warehouseHeight - 1; i++ ) {
-                    for( int j = 1; j < warehouseWidth - 1; j++ ) {
-                        if( !target.equals(warehouse[i][j]) ) {
-                            continue;
-                        }
-                           
-                        warehouse[i][j] = ".";
-                    }
-                }
+        int max = 0;
+        for( int i = 0; i < robotsPositionList.length; i++ ) {
+            if( max < robotsPositionList[i].size() ) {
+                max = robotsPositionList[i].size();
             }
         }
         
-        for( int i = 1; i < warehouseHeight - 1; i++ ) {
-            for( int j = 1; j < warehouseWidth - 1; j++ ) {
-                if( ".".equals(warehouse[i][j]) ) {
+        for( int i = 0; i < max; i++ ) {
+            int[][] stage = new int[101][101];
+            for( int j = 0; j < robotsPositionList.length; j++ ) {
+                if( i >= robotsPositionList[j].size() ) {
                     continue;
                 }
-
-                answer++;
+                
+                int[] pos = robotsPositionList[j].get(i);
+                int y = pos[0];
+                int x = pos[1];
+                
+                stage[y][x]++;
+            }
+            
+            for( int j = 0; j < 101; j++ ) {
+                for( int k = 0; k < 101; k++ ) {
+                    if( stage[j][k] > 1 ) {
+                        answer++;
+                    }
+                }
             }
         }
         
